8263d0ad29320982bd4a8867ab5d2ada5efc01e0
apanisile <ifeoluwa.apanisile@payinvert.com>
2026-09-09 10:04:23 +0100

    style: adopt Instagram-inspired typography

--- src/templates.ts
+++ src/templates.ts
@@ -13,7 +13,7 @@ import {
 const fonts = `
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;700;800&family=Cormorant+Garamond:wght@400;500;600;700&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css2?family=Figtree:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
 `;
 
 function renderNav(links: NavLink[]): string {
